018127de525c36986991dc8a8234fe36fede6f5c
Anderson <andersonvaloes@gmail.com>
2017-09-12 22:17:24 -0300

    Adicionando Alguns métodos

--- Projeto2Busca1/src/NRainhas.java
+++ Projeto2Busca1/src/NRainhas.java
@@ -1,11 +1,56 @@
+import java.util.Random;
 
 public class NRainhas {
+	
 	public final int N = 10;
-	int[] vet = new int[N];
+	int[][] vet;
+	int[][] pesos;
+	
+	public NRainhas() {
+		vet = new int[N][N];
+		pesos = new int[N][N];
+	}
+	
+	public void pesoHeuristica(int[][] v){
+		for(int i = 0; i < this.N; i++) {
+			
+		}
+	}
+	
+	
+	
+	public void generateRandomBoard() {
+		Random r = new Random();
+		int rand;
+		for(int i = 0; i < this.N; i++) {
+			rand = r.nextInt(this.N);
+			System.out.println(rand);
+			this.vet[i][rand] = -1;
+		}
+	}
+	
+	public void eraseAll() {
+		for(int i = 0; i < this.N; i++) {
+			for(int j = 0; j < this.N; j++) {
+				this.vet[i][j] = 0;
+			}
+		}
+	}
+	
+	public void printBoard() {
+		for(int i = 0; i < this.N; i++) {
+			for(int j = 0; j < this.N; j++) {
+				System.out.print(this.vet[i][j] + " ");	
+			}
+			System.out.println();
+		}
+	}
+	
 	public static void main(String[] args) {
-		NRainhas n = new NRainhas();
-		for(int i = 0; i < n.vet.length; i++)
-			System.out.println(n.vet[i]);
+		NRainhas nr = new NRainhas();
+		nr.eraseAll();
+		nr.generateRandomBoard();
+		nr.printBoard();
 	}
 	
 }
