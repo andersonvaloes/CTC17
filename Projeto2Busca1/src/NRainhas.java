c728352f1b80e8351351313c077e0bd3c57a903d
Anderson <andersonvaloes@gmail.com>
2017-09-13 17:44:27 -0300

    Método de calcular a heurística pronto

--- Projeto2Busca1/src/NRainhas.java
+++ Projeto2Busca1/src/NRainhas.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class NRainhas {
 	
-	public final int N = 10;
+	public final int N = 5;
 	int[][] vet;
 	int[][] pesos;
 	
@@ -11,10 +11,54 @@ public class NRainhas {
 		pesos = new int[N][N];
 	}
 	
-	public void pesoHeuristica(int[][] v){
-		for(int i = 0; i < this.N; i++) {
-			
+	public int pesoHeuristica(int[][] v){
+		int p = 0, q = 0;
+		for(int j = 0; j < this.N; j++) { //Coluna
+			for(int i = 0; i < this.N; i++) {
+				if(v[i][j] == -1) q++;
+			}
+			p = p + q*(q-1)/2;
+			q = 0;
 		}
+		//System.out.println(p + "Coluna");
+		
+		
+		for(int i = 0; i < this.N; i++) {//Diagonal \ inferior
+			for(int j = 0; i + j < this.N; j++) {
+				if(v[i+j][j] == -1) q++;
+			}
+			p = p + q*(q-1)/2;
+			q = 0;
+		}
+		//System.out.println(p + "Diagonal \\ inferior");
+		
+		for(int j = 1; j < this.N; j++) {//Diagonal \ superior
+			for(int i = 0; i + j < this.N; i++) {
+				if(v[i][i+j] == -1) q++;
+			}
+			p = p + q*(q-1)/2;
+			q = 0;
+		}
+		//System.out.println(p + "Diagonal \\ superior");
+		
+		for(int i = 0; i < this.N; i++) {//Diagonal / superior
+			for(int j = 0; i >= j; j++) {
+				if(v[i-j][j] == -1) q++;
+			}
+			p = p + q*(q-1)/2;
+			q = 0;
+		}
+		//System.out.println(p + "Diagonal / superior");
+		
+		for(int j = 1; j < this.N; j++) {//Diagonal / inferior
+			for(int i = 0; j + i < this.N; i++) {
+				if(v[this.N - i - 1][j + i] == -1) q++;
+			}
+			p = p + q*(q-1)/2;
+			q = 0;
+		}
+		//System.out.println(p + "Diagonal / inferior");
+		return p;
 	}
 	
 	
@@ -24,7 +68,7 @@ public class NRainhas {
 		int rand;
 		for(int i = 0; i < this.N; i++) {
 			rand = r.nextInt(this.N);
-			System.out.println(rand);
+			//System.out.println(rand);
 			this.vet[i][rand] = -1;
 		}
 	}
@@ -40,7 +84,8 @@ public class NRainhas {
 	public void printBoard() {
 		for(int i = 0; i < this.N; i++) {
 			for(int j = 0; j < this.N; j++) {
-				System.out.print(this.vet[i][j] + " ");	
+				if(this.vet[i][j] == -1) System.out.print("1 ");
+				else System.out.print(this.vet[i][j] + " ");	
 			}
 			System.out.println();
 		}
@@ -51,6 +96,7 @@ public class NRainhas {
 		nr.eraseAll();
 		nr.generateRandomBoard();
 		nr.printBoard();
+		System.out.println(nr.pesoHeuristica(nr.vet));
 	}
 	
 }
